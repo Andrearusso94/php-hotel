40dfc98944878e608f110c45a9a6fb7404032cf1
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-28 15:18:11 +0100

    add table bootstrap

--- index.php
+++ index.php
@@ -43,27 +43,74 @@ $hotels = [
 //stampo array 
 
 var_dump($hotels);
-foreach ($hotels as $hotel) {
-    foreach ($hotel as $key => $item) {
-        echo "" . $key . "  " . "" . $item;
-        echo "</br>";
-    }
-    echo "</hr>";
-}
+//foreach ($hotels as $hotel) {
+//foreach ($hotel as $key => $item) {
+//   echo "" . $key . "  " . "" . $item;
+//       echo "</br>";
+//  }
+//  echo "</hr>";
+//}
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php hotel</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
 </head>
 
 <body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="table-responsive-md">
+            <table class="table table-striped table-primary  align-middle">
+                <thead class="table-light">
+                    <caption>Table Name</caption>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Parking</th>
+                        <th>Vote</th>
+                        <th>Distance to Center</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <?php foreach ($hotels as $hotel) : ?>
 
+
+                        <tr class="table-primary">
+                            <?php foreach ($hotel as $key => $item) : ?>
+                                <td scope="row"><?php echo $item ?></td>
+                            <?php endforeach; ?>
+                        </tr>
+
+                    <?php endforeach; ?>
+                </tbody>
+                <tfoot>
+
+                </tfoot>
+            </table>
+        </div>
+
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
